43a145e1e20a7c9a89cdd59f4ab3855952efc509
rbaptiste23 <riley.bap@outlook.com>
2020-05-28 11:51:36 -0500

    Add PersonModel Class

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace MVCDemo.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Your contact page....";
 
             return View();
         }
